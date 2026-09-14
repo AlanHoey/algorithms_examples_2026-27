f10a85bc55a47674f43e196e67f95856cc4b8ae4
michelle <michelle.graham@dkit.ie>
2026-09-14 22:28:03 +0100

    docs: add comments to in-class example of i/o actions

--- src/main/java/in_class/DemoIO.java
+++ src/main/java/in_class/DemoIO.java
@@ -4,21 +4,32 @@ import java.util.Scanner;
 
 public class DemoIO {
     static void main() {
+        // Create scanner to allow us to read from the terminal/console
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your name: ");
+        // Prompt user
+        System.out.println("Please enter your username: ");
 
+        // Read in from terminal (input.nextLine()) and store in a variable
         String name = input.nextLine();
         System.out.println("Hello there, " + name);
 
+        // Check the name supplied against a fixed value
+        // ! means not - if the value entered is not "admin", tell them they're not allowed in
+        // Comparing to a string needs a method, as a String is an OBJECT
         if(!name.equals("admin")){
-            System.out.println("You're banned, get out!");
+            System.out.println("You're not meant to be here, get out!");
         }else {
-
+            // Otherwise, continue to age check
             System.out.println("Please enter your age: ");
+            // Read in age - different method (input.nextInt()) this time as we're reading in a number
             int age = input.nextInt();
 
+            // If the admin is under 18, they're too young
+            // Numeric comparison is done with > and < operators (like python)
+            // but the condition still needs to be within brackets
             if (age < 18) {
                 System.out.println("You're too young, get out!");
+                // If the age is under 25, limit their abilities
             } else if (age < 25) {
                 System.out.println("No car rental for you!");
             } else {
